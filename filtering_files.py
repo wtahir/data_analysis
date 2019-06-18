cf0f388e2dcac1c4c679ae1e116a0b08c1458424
waqast <waqas@omnius.com>
2019-06-18 14:11:01 +0200

    nlp added more commands

---
+++ filtering_files.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+
+def classes(txt_file):
+    files = pd.read_csv(txt_file, delim_whitespace=True)
+
+    files.columns = ['filename', 'class']
+    class_ = files.where(files['class'] == 1)
+    class_ = class_.dropna().iloc[300:400]
+    class_ = class_.drop(columns=['class'])
+    return class_.to_csv('class1.csv', sep=',', index=False, header=False)
+
+classes('/home/waqas/Desktop/data/images/train.txt')
